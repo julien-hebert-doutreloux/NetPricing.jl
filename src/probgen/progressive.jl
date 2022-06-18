c9ba35f6f3234982d43ccdb4ecf0fc73558811a7
Bui Quang Minh <minhcly95@outlook.com>
2022-06-17 21:32:16 -0400

    Readjust progressive generation using num edges

--- src/probgen/progressive.jl
+++ src/probgen/progressive.jl
@@ -30,8 +30,9 @@ function generate_progressive_grid(max_size, min_size, base_num_commodities, arg
         # Extract the induced subgraph (with the old costs of arcs)
         next_graph = graph[collect(subgraph_nodes)]
 
-        # Increase the number of commodities inversely-proportionally to the number of nodes
-        num_commodities = round(Int, base_num_commodities * (max_size * max_size) / (size * size))
+        # Increase the number of commodities inversely-proportionally to the number of edges
+        # The number of edges is 2n(n-1) where n is the size of the grid
+        num_commodities = round(Int, base_num_commodities * (max_size * (max_size - 1)) / (size * (size - 1)))
         last_K = prob.K
         next_K = Commodity[]
 
@@ -67,56 +68,57 @@ function generate_progressive_grid(max_size, min_size, base_num_commodities, arg
             push!(next_K, Commodity(o, d, rand(demand_dist)))
         end
 
-        # Try to reuse the tolled arcs
-        num_arcs = ne(next_graph)
-        num_tolled_arcs = round(Int, num_arcs * tolled_proportion)
+        # # Try to reuse the tolled arcs
+        # num_arcs = ne(next_graph)
+        # num_tolled_arcs = round(Int, num_arcs * tolled_proportion)
 
-        candidates = [(arc.src, arc.dst) for arc in prob.A if arc.toll] # Extract tolled arcs from previous problem
-        filter!(candidates) do (i, j)                                   # Filter out the arcs that are not in the subgraph
-            return i in subgraph_nodes && j in subgraph_nodes
-        end
-        map!(candidates, candidates) do (i, j)                          # Map to subgraph's node indices
-            return (node_map[i], node_map[j])
-        end
-        shuffle!(candidates)
+        # candidates = [(arc.src, arc.dst) for arc in prob.A if arc.toll] # Extract tolled arcs from previous problem
+        # filter!(candidates) do (i, j)                                   # Filter out the arcs that are not in the subgraph
+        #     return i in subgraph_nodes && j in subgraph_nodes
+        # end
+        # map!(candidates, candidates) do (i, j)                          # Map to subgraph's node indices
+        #     return (node_map[i], node_map[j])
+        # end
+        # shuffle!(candidates)
 
-        # Append the remaining arcs to candidates in random order
-        remaining = [(src(e), dst(e)) for e in edges(next_graph)]
-        setdiff!(remaining, candidates)
-        shuffle!(remaining)
-        append!(candidates, remaining)
+        # # Append the remaining arcs to candidates in random order
+        # remaining = [(src(e), dst(e)) for e in edges(next_graph)]
+        # setdiff!(remaining, candidates)
+        # shuffle!(remaining)
+        # append!(candidates, remaining)
 
-        # Add the candidates (old tolled arcs first, then other random arcs) if they do not disconnect any o-d pair
-        tollfreegraph = SimpleDiGraph(next_graph)
-        tolled = Set{Tuple{Int,Int}}()
+        # # Add the candidates (old tolled arcs first, then other random arcs) if they do not disconnect any o-d pair
+        # tollfreegraph = SimpleDiGraph(next_graph)
+        # tolled = Set{Tuple{Int,Int}}()
 
-        reverse!(candidates)    # Reverse the order so we can use pop!
-        while !isempty(candidates)
-            i, j = pop!(candidates)
-            # Already in tolled
-            (i, j) in tolled && continue
+        # reverse!(candidates)    # Reverse the order so we can use pop!
+        # while !isempty(candidates)
+        #     i, j = pop!(candidates)
+        #     # Already in tolled
+        #     (i, j) in tolled && continue
 
-            # Must be removable (does not disconnect any commodity)
-            _is_removable(tollfreegraph, i, j, next_K) || continue
-            # So does the reversed arc if symmetric_tolled
-            if symmetric_tolled && has_edge(tollfreegraph, j, i)
-                _is_removable(tollfreegraph, j, i, next_K) || continue
-            end
+        #     # Must be removable (does not disconnect any commodity)
+        #     _is_removable(tollfreegraph, i, j, next_K) || continue
+        #     # So does the reversed arc if symmetric_tolled
+        #     if symmetric_tolled && has_edge(tollfreegraph, j, i)
+        #         _is_removable(tollfreegraph, j, i, next_K) || continue
+        #     end
     
-            # If OK, add to tolled
-            push!(tolled, (i, j))
-            rem_edge!(tollfreegraph, i, j)
-            if symmetric_tolled && has_edge(tollfreegraph, j, i)
-                push!(tolled, (j, i))
-                rem_edge!(tollfreegraph, j, i)
-            end
+        #     # If OK, add to tolled
+        #     push!(tolled, (i, j))
+        #     rem_edge!(tollfreegraph, i, j)
+        #     if symmetric_tolled && has_edge(tollfreegraph, j, i)
+        #         push!(tolled, (j, i))
+        #         rem_edge!(tollfreegraph, j, i)
+        #     end
 
-            # Total target reached: break
-            length(tolled) >= num_tolled_arcs && break
-        end
+        #     # Total target reached: break
+        #     length(tolled) >= num_tolled_arcs && break
+        # end
 
         # Add the tolled arcs (from scratch)
-        A = _convert_to_A(next_graph, tolled)
+        A = generate_tolled_arcs(next_graph, next_K, args)
+        # A = _convert_to_A(next_graph, tolled)
 
         # Update variables for the next iterations
         prob = Problem(V, A, next_K)

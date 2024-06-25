ebb16b8969a9b0b717f26a16683ba544fbb76535
fiftyfour <julien.hebert-doutreloux@umontreal.ca>
2024-06-25 12:12:52 -0400

    update

--- src/models/formulation.jl
+++ src/models/formulation.jl
@@ -47,7 +47,71 @@ end
 
 
 # Formulate: actualize all assigned formulations into model (CUSTOM)
-function custom_formulate!(forms::Vector{<:Formulation}, linearization::AbstractLinearization, Ms, NL, option; silent=false, threads=nothing, sdtol=1e-10, kwargs...)
+# function custom_formulate!(forms::Vector{<:Formulation}, linearization::AbstractLinearization, Ms, NL, option; silent=false, threads=nothing, sdtol=1e-10, kwargs...)
+#     model = Model(() -> Gurobi.Optimizer(current_env())) # NetPricing.
+#     set_optimizer_attribute(model, MOI.Silent(), silent)
+#     set_optimizer_attribute(model, MOI.NumberOfThreads(), threads)
+
+#     prob = problem(first(forms))
+
+#     # Big M
+#     #Ms = [calculate_bigM(form, threads=threads) for form in forms]
+#     NU = max.(collect.(maximum.(Ms, dims=2))...)
+
+#     # Toll variables
+#     a1 = tolled_arcs(prob)
+#     a1dict = Dict(a => i for (i, a) in enumerate(a1))
+    
+#     if option == 0
+# 		# normal
+# 		@variable(model, 0 ≤ t[a=a1], upper_bound = NU[a1dict[a]])
+# 		N = NU
+# 		#println("option 0")
+#     elseif option == 1
+#     	# shortest path
+#     	@variable(model, t[a=a1], lower_bound = NL[a1dict[a]], upper_bound = NL[a1dict[a]])
+# 		N = NU
+# 		#println("option 1")
+# 	elseif option == 2
+# 		# lower bound
+# 		@variable(model, t[a=a1], lower_bound = NL[a1dict[a]], upper_bound = NU[a1dict[a]])
+# 		N = NU
+# 		#println("option 2")
+# 	elseif option == 3 
+# 		# upper bound
+# 		@variable(model, 0 ≤ t[a=a1], upper_bound = NL[a1dict[a]])
+# 		N = NL
+# 		#println("option 3")
+# 	elseif option == 4
+# 		# comprehensive lower bound
+# 		L = Int.(NL .< NU) .* NL
+# 		@variable(model, t[a=a1], lower_bound = L[a1dict[a]], upper_bound = NU[a1dict[a]])
+# 		N = NU
+# 		#println("option 4")
+# 	elseif option == 5
+# 		# comprehensive upper bound
+# 		U = Int.(NL .< NU) .* NL + Int.(NU .<= NL) .* NU
+# 		@variable(model, 0 ≤ t[a=a1], upper_bound = U[a1dict[a]])
+# 		N = U
+# 		#println("option 5")
+# 	end
+	
+#     # Add formulations
+#     for form in forms
+#         append!(model, form; kwargs...)
+#     end
+
+#     # Linearize (and strong duality)
+#     linearize!(model, linearization, forms, Ms, N; sdtol=sdtol) #NetPricing.
+
+#     # Set objective function
+#     @objective(model, Max, sum(objective_term.(forms))) # NetPricing.
+
+#     return model, forms
+# end
+
+
+function formulate!(forms::Vector{<:Formulation}, linearization::AbstractLinearization, Ms, NL, option; rtrans=nothing, trans=nothing, silent=false, threads=nothing, sdtol=1e-10, kwargs...)
     model = Model(() -> Gurobi.Optimizer(current_env())) # NetPricing.
     set_optimizer_attribute(model, MOI.Silent(), silent)
     set_optimizer_attribute(model, MOI.NumberOfThreads(), threads)
@@ -95,17 +159,63 @@ function custom_formulate!(forms::Vector{<:Formulation}, linearization::Abstract
 		N = U
 		#println("option 5")
 	end
+	elseif option == 6 & rtrans != nothing & trans != nothing
+		
+		c  = cost_vector(prob)      # constant cost vector from the original problem
+		γc = projection(etrans, c)  # Projection of c in transformed space
+				
+		γA = trans["M_"]    # incidence matrix in transformed space
+		γA = hcat(M_...)'   # convert in a matrix (vertex, edge)
+
+		nv_ = size(γA)[1]   # number of node in the transformed space
+		na_ = size(γA)[2]   # number of arcs in the transformed space
+		###############
+		
+		# in kwargs		
+		vtrans = trans["V"]# Vertex transformation (before:after)
+		etrans = trans ["A"]# Edge transformation (before:after)
+		#rtrans # result from a transformed problem
+
+		ktrans = Dict()     # mapping between full b~ and its associated index k (b~:after)
+		# Only works for result in transformed space (when x is in the id)
+		for (k, v) in rtrans.b
+			VV = rtrans.Vmap[k]
+			bfull = expand_b(VV, nv_, v) 
+			ktrans[bfull] = k
+		end
+
+		γa1 = []         # tolled edge index in the transformed space
+		γa1dict = Dict() # mapping entre indices et classes d'équivalences d'indice
+		for e in trans["RA"]
+			if !isempty(intersect(e, a1))
+				i = etrans[string(e[1])]
+				append!(γa1, i)
+				γa1dict[i] = e
+			end
+		end
+				
+		# γ(A), λ~, γ(c), c, b, γ^-1(λ~), x~, γ(b) sont des constantes
+		# γ(A)' * λ~ <= γ(c) + γ(t) need linearize_commodity_primal_custom
+		# (c + t)' * x <= b' * γ^-1(λ~) need linearize_commodity_primal_custom
+		# (γ(c) + γ(t))' * x~ <= γ(b)' * λ~ need linearize_commodity_primal_custom
+	end
 	
     # Add formulations
     for form in forms
         append!(model, form; kwargs...)
     end
 
-    # Linearize (and strong duality)
-    linearize!(model, linearization, forms, Ms, N; sdtol=sdtol) #NetPricing.
-
+	if option==6
+		custom_linearize!(model, linearization, forms, Ms, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1; sdtol=sdtol)
+	else
+		# Linearize (and strong duality)
+		linearize!(model, linearization, forms, Ms, N; sdtol=sdtol) #NetPricing.
+	end
+	
+	
     # Set objective function
     @objective(model, Max, sum(objective_term.(forms))) # NetPricing.
 
     return model, forms
 end
+
--- src/models/linearizations/commodity-linearization.jl
+++ src/models/linearizations/commodity-linearization.jl
@@ -76,3 +76,93 @@ linearize_commodity_extra(::PathLinearization, primal::PrimalArc) =
 struct EnvelopOnly <: CommodityLinearization end
 
 function linearize_commodity_extra(::EnvelopOnly, ::PrimalRepresentation) end
+
+
+
+########################## CUSTOM
+
+#### Custom
+function custom_linearize!(model::Model, linearization::CommodityLinearization, forms, Ms, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1; sdtol=1e-10)
+    for (form, M) in zip(forms, Ms)
+        custom_linearize_commodity!(model, linearization, form, M, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1 sdtol=sdtol)
+    end
+    return
+end
+
+function custom_linearize_commodity!(model::Model, linearization::CommodityLinearization, form::Formulation, M, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1; sdtol=1e-10)
+    # Linearization
+    sumtx = custom_linearize_commodity_primal(model, linearization, primal(form), M, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1)
+    # Strong duality
+    @constraint(model, sumtx <= unnormalized_objective_term(form) + sdtol)
+    return sumtx
+end
+
+function custom_linearize_commodity_primal(model::Model, linearization::CommodityLinearization, primal::PrimalRepresentation, M, N, rtrans, vtrans, ktrans, γa1dict, nv_,na_, c, γc, γA, γa1)
+
+    prob = problem(primal)
+    parentprob = parent(prob)
+
+    a1 = tolled_arcs(prob)
+    k = index(prob)
+    Amap = arcmap(prob)
+	Vmap = nodemap(prob)
+	
+    x = primal.x[a1]
+    tx = @variable(model, [a=a1], lower_bound = 0, base_name="tx[$k]")
+    t = remap_t(model, prob)
+
+    if isempty(tx)
+        sumtx = 0.0
+    else
+        sumtx = sum(tx)
+    end
+
+    a1dict = Dict(a => i for (i, a) in enumerate(tolled_arcs(parentprob)))
+    mapped_a1 = [a1dict[a] for a in Amap[a1]]
+    M = @view M[mapped_a1]
+    N = @view N[mapped_a1]
+
+    # Linearization
+    @constraint(model, tx .≤ M .* x)
+    @constraint(model, t .- tx .≥ 0)
+    @constraint(model, t .- tx .≤ N .* (1 .- x))
+    #########################################################
+
+
+	# variable artificiel γt
+	@variable(model, γt[a=γa1], base_name="γt[$k]") # pas besoin de borner voir les contraintes
+	for (k, v) in γa1dict
+		@constraint(model, γt[k] == mean(t[v]))
+	end
+
+	
+    b = sourcesink_vector(prob)			# Source sink vector 
+    nv = length(nodes(prob))			# number of nodes
+ 	bfull = expand_b(Vmap, nv, b)		# Source sink vector in full dimension
+ 	γbfull = projection(vtrans, bfull)	# Projected Source sink vector full dimension
+ 	
+ 	if γbfull in ktrans                 # it is possible that the problem become infeasible in the transformed space
+ 				
+		k_ = ktrans[projection(vtrans, bfull)]               # retrieve the associated transformed problem
+		λ_ = rtrans.λvals[k_];                              # Reduce dual value in transformed space with the corresponding problem
+		λ_full = expand_b(rtrans.Vmap[k_], nv_, λ_)         # Full dimension dual value in transformed space
+		γ_inv_λ_full = retroprojection(vtrans, value.(λ_full)) # Retroprojection of the dual value into the original space
+		
+		x_ = rtrans.xvals[k_];
+		x_full = expand_b(rtrans.Amap[k_], na_, x_)       # optimal solution path in transformed problem 
+
+		# γ(A)' * λ~ <= γ(c) + γ(t)
+		@constraint(model, γA' * λ_full .≤ γc + (γt))
+		# (c + t)' * x <= b' * γ^-1(λ~)
+		@constraint(model, c' * x + sumtx ≤ b' * γ_inv_λ_full)
+		# (γ(c) + γ(t))' * x~ <= γ(b)' * λ~
+		@constraint(model, (γc + γt)' *  x_full ≤ γbfull' * λ_full)
+		
+    
+
+    # Run linearization-specific code
+    linearize_commodity_extra(linearization, primal)
+
+    return sumtx
+end
+
